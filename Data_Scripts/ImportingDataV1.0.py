b0afcb1240a58fc5d14a0b06e07c38c30c79b14a
Dylan Kierans <kieransd@tcd.ie>
2021-11-29 14:47:37 +0000

    kNN model added in V1.0

---
+++ Data_Scripts/ImportingDataV1.0.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 25/11/21
+# Machine Learning Group Project
+# 
+# 
+# This file is our main file for the project.  All of our code will be contained
+# here.
+# 
+# 
+# Initial file creation by Dylan Kierans
+
+# # Version 0.1
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rc("font", size=14)
+
+
+# # Checking csv and titles by importing first row of a file
+
+# In[2]:
+
+
+file_dir="../SpotifyData/"
+file_name="ClassicRock_3Ho3iO0iJykgEQNbjB2sic.csv"
+
+test_csv = pd.read_csv(file_dir+file_name, index_col=0, nrows=1) 
+display(test_csv)
+
+
+# # Choosing input columns
+# Now just importing the necessary training parameters
+
+# In[3]:
+
+
+#Sticking with just the basic parameters for now
+parameter_columns = [1,2,4,5,7,8,9]
+
+#Saving these other ones for later
+# key_column=3
+# tempo_column=11
+# duration_column=17
+
+
+#Testing to make sure read column numbers correctly
+data = pd.read_csv(file_dir+file_name, usecols=parameter_columns) 
+display(data.head())
+
+
+# # Playlist_number Column
+# Now to add an extra column which will hold the playlist_number
+
+# In[4]:
+
+
+# Can never be too safe with printing len() values
+print(len(data))
+
+#Want to add column with name "playlist_number" and all entries equal to playlist number
+playlist_number=1
+playlist_numbers = playlist_number*np.ones(len(data))
+
+data["playlist_number"]=playlist_numbers
+display(data)
+#Works a charm
+
+
+# # Compiling Data
+# Next step is to make a single dataframe holding the data from all source files
+
+# In[5]:
+
+
+file_names= ["ClassicRock_3Ho3iO0iJykgEQNbjB2sic.csv",             "ClassicalStudy_6wObnEPQ63a4kei1sEcMdH.csv",             "CountryHits_1mJhSx6aYQmINsZ8dG4gzU.csv",             "DanceHits_5oKz4DsTP8zbL97UIPbqp4.csv",             "HeavyMetalClassics_27gN69ebwiJRtXEboL12Ih.csv",             "HipHopClassics_5CRJExGSQFYXem2hT5GFkd.csv",             "JazzClassic_37i9dQZF1DXbITWG1ZJKYt.csv",             "KpopClassics_0jB4ANR4ox65etDMnxvGLp.csv"]
+
+
+# Most convenient to set up dataframe with initial file, then systematically loop through the others
+#Sticking with just the basic parameters for now
+parameter_columns = [1,2,5,6,7,8,9]
+nrows=200
+
+
+
+#1 indexing here more natural
+playlist_number=1
+data = pd.read_csv(file_dir+file_names[playlist_number-1], usecols=parameter_columns, nrows=nrows)
+
+playlist_numbers = playlist_number*np.ones(len(data))
+data["playlist_number"]=playlist_numbers #Now has extra playlist column
+
+
+
+# Now looping through the remaining filenames
+for playlist_number in range(2,len(file_names)+1):
+    # dtmp := Temporary dataframe, will append to df.data once data filtered and prepped
+    dtmp= pd.read_csv(file_dir+file_names[playlist_number-1], usecols=parameter_columns, nrows=nrows)
+
+    playlist_numbers = playlist_number*np.ones(len(dtmp)) # array of playlist_number
+    dtmp["playlist_number"]=playlist_numbers #Now has extra playlist column
+    data=data.append(dtmp, ignore_index=True) #Append back to main df.data
+    
+display(data)
+
+
+# # Training?
+# Any pre-processing necessary for:
+# - Repeat songs
+# - Incomplete data
+# 
+# 
+# Note: I have only chosen 200 songs from each playlist, some do contain more songs
+# 
+
+# # kNN Model
+
+# In[6]:
+
+
+X = data.loc[:,"danceability":"liveness"]
+Y = data.loc[:,"playlist_number"]
+
+from sklearn.neighbors import KNeighborsClassifier
+
+model = KNeighborsClassifier(n_neighbors=3, weights="uniform").fit(X,Y)
+ypred = model.predict(X)
+
+
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(ypred,Y))
+
+
+# # Precision for uniform kNN Weights
+
+# In[7]:
+
+
+X = data.loc[:,"danceability":"liveness"].values
+Y = data.loc[:,"playlist_number"]
+
+n_splits = 5
+ks=[3,5,7,9]
+
+from sklearn.metrics import roc_curve
+from sklearn.model_selection import KFold    
+from sklearn.metrics import precision_score
+kf = KFold(n_splits= n_splits, shuffle=True) #Important to shuggle our ordered data
+
+
+tmp=[]
+precision=[]
+precision_err=[]
+for k in ks:
+    model = KNeighborsClassifier(n_neighbors=k, weights="uniform")
+    for train, test in kf.split(X):
+        model.fit(X[train],Y[train])
+        ypred = model.predict(X[test])
+        #print(confusion_matrix(ypred,Y[test]))
+
+        tmp.append( precision_score(Y[test], ypred, average="weighted") )
+        ## average = {‘micro’, ‘macro’, ‘samples’, ‘weighted’, ‘binary’}
+
+    precision.append( np.array(tmp).mean() )
+    precision_err.append( np.array(tmp).std() )
+
+print(precision)
+print(precision_err)
+
+plt.errorbar(ks, precision, yerr=precision_err)
+plt.xlabel("k")
+plt.ylabel("Precision")
+plt.title("Precision for weight average, kNN uniform")
+plt.show()
+
+
+# # Precision for distance kNN weights
+
+# In[8]:
+
+
+tmp=[]
+precision=[]
+precision_err=[]
+for k in ks:
+    model = KNeighborsClassifier(n_neighbors=k, weights="distance")
+    for train, test in kf.split(X):
+        model.fit(X[train],Y[train])
+        ypred = model.predict(X[test])
+        #print(confusion_matrix(ypred,Y[test]))
+
+        tmp.append( precision_score(Y[test], ypred, average="weighted") )
+        ## average = {‘micro’, ‘macro’, ‘samples’, ‘weighted’, ‘binary’}
+
+    precision.append( np.array(tmp).mean() )
+    precision_err.append( np.array(tmp).std() )
+
+print(precision)
+print(precision_err)
+
+plt.errorbar(ks, precision, yerr=precision_err)
+plt.xlabel("k")
+plt.ylabel("Precision")
+plt.title("Precision for weight average, kNN distance")
+plt.show()
+
+
+# # Accuracy for distance kNN weights
+
+# In[34]:
+
+
+prec_tmp=[]
+precision=[]
+precision_err=[]
+score_tmp=[]
+score=[]
+score_err=[]
+for k in ks:
+    model = KNeighborsClassifier(n_neighbors=k, weights="distance")
+    
+    score_tmp=[]
+    for train, test in kf.split(X):
+        model.fit(X[train],Y[train])
+        ypred = model.predict(X[test])
+        #print(confusion_matrix(ypred,Y[test]))
+        score_tmp.append(model.score(X[test],Y[test]))#accuracy score
+    score.append(np.array(score_tmp).mean())
+    score_err.append(np.array(score_tmp).std())
+    
+print(score)
+print(score_err)
+
+plt.errorbar(ks, score, yerr=score_err)
+plt.xlabel("k")
+plt.ylabel("Accuracy")
+plt.title("Score kNN (weighted by distance)")
+plt.show()
+
+
+# In[ ]:
+
+
+
+
